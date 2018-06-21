ab3abee9b033d26d7b5398d3fe037bf9aba736ee
Kolya Grebnev <mumiyasss@gmail.com>
2018-06-21 19:02:59 +0300

    Username displays in main screen now

--- FlappyBirdForMW/android/src/com/milkyway/flappybird/pages/LoginActivity.java
+++ FlappyBirdForMW/android/src/com/milkyway/flappybird/pages/LoginActivity.java
@@ -20,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -70,11 +71,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signIn = (Button)findViewById(R.id.email_sign_in_button) ;
+        signIn = findViewById(R.id.email_sign_in_button) ;
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TextView usernameForSharedPref = findViewById(R.id.username);
+                Log.d("us", usernameForSharedPref.getText().toString());
                 Intent i = new Intent(LoginActivity.this, MainMenu.class);
+                i.putExtra("username", usernameForSharedPref.getText().toString());
                 startActivity(i);
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Вход выполнен успешно!", Toast.LENGTH_SHORT);
@@ -82,7 +86,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -97,7 +101,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-      /*  Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+       /*  Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
--- FlappyBirdForMW/android/src/com/milkyway/flappybird/pages/MainMenu.java
+++ FlappyBirdForMW/android/src/com/milkyway/flappybird/pages/MainMenu.java
@@ -3,8 +3,10 @@ package com.milkyway.flappybird.pages;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.milkyway.flappybird.R;
 
@@ -15,11 +17,15 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        flappyBird = (Button)findViewById(R.id.flappy_bird);
-        arithmetic = (Button)findViewById(R.id.arithmetic);
-        puzzle = (Button)findViewById(R.id.puzzle);
-        exit = (Button)findViewById(R.id.exit);
-        statistics = (Button)findViewById(R.id.statistics);
+        String username = getIntent().getExtras().getString("username");
+        Log.d("extra", username);
+        flappyBird = findViewById(R.id.flappy_bird);
+        arithmetic = findViewById(R.id.arithmetic);
+        puzzle = findViewById(R.id.puzzle);
+        exit = findViewById(R.id.exit);
+        statistics = findViewById(R.id.statistics);
+        TextView textView = findViewById(R.id.username_main_screen);
+        textView.setText(username);
         flappyBird.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
